ba2a73fc579d5ecbc218ce5339080888212abe13
Jonathan <jdgelbart@gmail.com>
2021-07-03 14:34:42 -0700

    First commit

---
+++ Set4a.hs
@@ -0,0 +1,249 @@
+-- Exercise set 4a:
+--
+-- * using type classes
+-- * working with lists
+--
+-- Type classes you'll need
+--  * Eq
+--  * Ord
+--  * Num
+--  * Fractional
+--
+-- Useful functions:
+--  * maximum
+--  * minimum
+--  * sort
+
+module Set4a where
+
+import Mooc.Todo
+import Data.List
+import Data.Ord
+import qualified Data.Map as Map
+import Data.Array
+
+------------------------------------------------------------------------------
+-- Ex 1: implement the function allEqual which returns True if all
+-- values in the list are equal.
+--
+-- Examples:
+--   allEqual [] ==> True
+--   allEqual [1,2,3] ==> False
+--   allEqual [1,1,1] ==> True
+--
+-- PS. check out the error message you get with your implementation if
+-- you remove the Eq a => constraint from the type!
+
+allEqual :: Eq a => [a] -> Bool
+allEqual [] = True
+allEqual [x] = True
+allEqual (x1:x2:xs) = (x1 == x2) && allEqual xs
+
+------------------------------------------------------------------------------
+-- Ex 2: implement the function distinct which returns True if all
+-- values in a list are different.
+--
+-- Hint: a certain function from the lecture material can make this
+-- really easy for you.
+--
+-- Examples:
+--   distinct [] ==> True
+--   distinct [1,1,2] ==> False
+--   distinct [1,2] ==> True
+
+distinct :: Eq a => [a] -> Bool
+distinct xs = nub xs == xs
+
+------------------------------------------------------------------------------
+-- Ex 3: implement the function middle that returns the middle value
+-- (not the smallest or the largest) out of its three arguments.
+--
+-- The function should work on all types in the Ord class. Give it a
+-- suitable type signature.
+--
+-- Examples:
+--   middle 'b' 'a' 'c'  ==> 'b'
+--   middle 1 7 3        ==> 3
+
+middle :: Ord a => a -> a -> a -> a
+middle a b c = sort [a, b, c] !! 1
+
+------------------------------------------------------------------------------
+-- Ex 4: return the range of an input list, that is, the difference
+-- between the smallest and the largest element.
+--
+-- Your function should work on all suitable types, like Float and
+-- Int. You'll need to add _class constraints_ to the type of range.
+--
+-- It's fine if your function doesn't work for empty inputs.
+--
+-- Examples:
+--   rangeOf [4,2,1,3]          ==> 3
+--   rangeOf [1.5,1.0,1.1,1.2]  ==> 0.5
+
+rangeOf :: (Num a, Ord a) => [a] -> a
+rangeOf xs = foldr max (head xs) (tail xs) - foldr min (head xs) (tail xs)
+
+------------------------------------------------------------------------------
+-- Ex 5: given a (non-empty) list of (non-empty) lists, return the longest
+-- list. If there are multiple lists of the same length, return the list that
+-- has the smallest _first element_.
+--
+-- (If multiple lists have the same length and same first element,
+-- you can return any one of them.)
+--
+-- Give the function "longest" a suitable type.
+--
+-- Challenge: Can you solve this exercise without sorting the list of lists?
+--
+-- Examples:
+--   longest [[1,2,3],[4,5],[6]] ==> [1,2,3]
+--   longest ["bcd","def","ab"] ==> "bcd"
+
+longest :: Ord a => [[a]] -> [a]
+longest [] = []
+longest (x:xs) = foldr getLonger x xs where
+    getLonger a b = case comparing length a b of
+        LT -> b
+        GT -> a
+        EQ -> case compare (head a) (head b) of
+            LT -> a
+            GT -> b
+            EQ -> def
+        where def = a   -- arbitrary
+
+------------------------------------------------------------------------------
+-- Ex 6: Implement the function incrementKey, that takes a list of
+-- (key,value) pairs, and adds 1 to all the values that have the given key.
+--
+-- You'll need to add _class constraints_ to the type of incrementKey
+-- to make the function work!
+--
+-- The function needs to be generic and handle all compatible types,
+-- see the examples.
+--
+-- Examples:
+--   incrementKey True [(True,1),(False,3),(True,4)] ==> [(True,2),(False,3),(True,5)]
+--   incrementKey 'a' [('a',3.4)] ==> [('a',4.4)]
+
+incrementKey :: (Ix k, Num v) => k -> [(k,v)] -> [(k,v)]
+incrementKey keyCheck = map updateIfKey where
+    updateIfKey (key, val) = (key, newVal) where
+        newVal = if key == keyCheck then val + 1 else val
+
+------------------------------------------------------------------------------
+-- Ex 7: compute the average of a list of values of the Fractional
+-- class.
+--
+-- There is no need to handle the empty list case.
+--
+-- Hint! since Fractional is a subclass of Num, you have all
+-- arithmetic operations available
+--
+-- Hint! you can use the function fromIntegral to convert the list
+-- length to a Fractional
+
+average :: Fractional a => [a] -> a
+average xs = sum xs / fromIntegral (length xs)
+
+------------------------------------------------------------------------------
+-- Ex 8: given a map from player name to score and two players, return
+-- the name of the player with more points. If the players are tied,
+-- return the name of the first player (that is, the name of the
+-- player who comes first in the argument list, player1).
+--
+-- If a player doesn't exist in the map, you can assume they have 0 points.
+--
+-- Hint: Map.findWithDefault can make this simpler
+--
+-- Examples:
+--   winner (Map.fromList [("Bob",3470),("Jane",2130),("Lisa",9448)]) "Jane" "Lisa"
+--     ==> "Lisa"
+--   winner (Map.fromList [("Mike",13607),("Bob",5899),("Lisa",5899)]) "Lisa" "Bob"
+--     ==> "Lisa"
+
+winner :: Map.Map String Int -> String -> String -> String
+winner scores player1 player2 = compareScores (getScore player1) (getScore player2) where
+    getScore p = Map.findWithDefault 0 p scores
+    compareScores first second = case compare first second of
+      LT -> player2
+      GT -> player1
+      EQ -> player1
+
+------------------------------------------------------------------------------
+-- Ex 9: compute how many times each value in the list occurs. Return
+-- the frequencies as a Map from value to Int.
+--
+-- Challenge 1: try using Map.alter for this
+--
+-- Challenge 2: use foldr to process the list
+--
+-- Example:
+--   freqs [False,False,False,True]
+--     ==> Map.fromList [(False,3),(True,1)]
+
+freqs :: (Eq a, Ord a) => [a] -> Map.Map a Int
+freqs xs = foldr updateCounts startMap xs where
+    alterFunc (Just i) = Just (i + 1)
+    alterFunc Nothing = Nothing
+    updateCounts k map = Map.alter alterFunc k map
+    startMap = Map.fromList [(val, 0) | val <- xs]
+
+-- accumArray is an interesting option here too
+
+------------------------------------------------------------------------------
+-- Ex 10: recall the withdraw example from the course material. Write a
+-- similar function, transfer, that transfers money from one account
+-- to another.
+--
+-- However, the function should not perform the transfer if
+-- * the from account doesn't exist,
+-- * the to account doesn't exist,
+-- * the sum is negative,
+-- * or the from account doesn't have enough money.
+--
+-- Hint: there are many ways to implement this logic. Map.member or
+-- Map.notMember might help.
+--
+-- Examples:
+--   let bank = Map.fromList [("Bob",100),("Mike",50)]
+--   transfer "Bob" "Mike" 20 bank
+--     ==> fromList [("Bob",80),("Mike",70)]
+--   transfer "Bob" "Mike" 120 bank
+--     ==> fromList [("Bob",100),("Mike",50)]
+--   transfer "Bob" "Lisa" 20 bank
+--     ==> fromList [("Bob",100),("Mike",50)]
+--   transfer "Lisa" "Mike" 20 bank
+--     ==> fromList [("Bob",100),("Mike",50)]
+
+transfer :: String -> String -> Int -> Map.Map String Int -> Map.Map String Int
+transfer from to amount bank = if isValid then newBank else bank where
+    fromBalance = Map.findWithDefault 0 from bank
+    toBalance = Map.findWithDefault 0 to bank
+    isValid = Map.member from bank && Map.member to bank && amount >= 0 && 
+        fromBalance >= amount
+    newBank = Map.insert from (fromBalance - amount) (Map.insert to (toBalance + amount) bank)
+------------------------------------------------------------------------------
+-- Ex 11: given an Array and two indices, swap the elements in the indices.
+--
+-- Example:
+--   swap 2 3 (array (1,4) [(1,"one"),(2,"two"),(3,"three"),(4,"four")])
+--         ==> array (1,4) [(1,"one"),(2,"three"),(3,"two"),(4,"four")]
+
+swap :: Ix i => i -> i -> Array i a -> Array i a
+swap i j arr = arr // [(i, arr ! j), (j, arr ! i)]
+
+------------------------------------------------------------------------------
+-- Ex 12: given an Array, find the index of the largest element. You
+-- can assume the Array isn't empty.
+--
+-- You may assume that the largest element is unique.
+--
+-- Hint: check out Data.Array.indices or Data.Array.assocs
+
+maxIndex :: (Ix i, Ord a) => Array i a -> i
+maxIndex arr = foldr largestIdx firstVal indexList where
+    indexList = Data.Array.indices arr
+    firstVal = head indexList
+    largestIdx currIdx runningIdx = 
+        if (arr ! currIdx) > (arr ! runningIdx) then currIdx else runningIdx
---
+++ Set6.hs
@@ -0,0 +1,282 @@
+-- Exercise set 6: defining classes and instances
+
+module Set6 where
+
+import Mooc.Todo
+import Data.Char (toLower)
+
+------------------------------------------------------------------------------
+-- Ex 1: define an Eq instance for the type Country below. You'll need
+-- to use pattern matching.
+
+data Country = Finland | Switzerland | Norway
+  deriving Show
+
+instance Eq Country where
+  Finland == Finland = True
+  Switzerland == Switzerland = True
+  Norway == Norway = True
+  _ == _ = False
+
+------------------------------------------------------------------------------
+-- Ex 2: implement an Ord instance for Country so that
+--   Finland <= Norway <= Switzerland
+--
+-- Remember minimal complete definitions!
+
+instance Ord Country where
+  -- compare = todo -- implement me?
+  (<=) a b
+    | (a == Finland || a == Norway) && (b == Norway || b == Switzerland) = True
+    | a == Switzerland && (b == Norway || b == Finland) = False
+    | a == Norway && b == Switzerland = True
+    | a == Norway && b == Finland = False
+    | a == b = True
+    | otherwise = False
+  -- min = todo -- and me?
+  -- max = todo -- and me?
+
+------------------------------------------------------------------------------
+-- Ex 3: Implement an Eq instance for the type Name which contains a String.
+-- The Eq instance should ignore capitalization.
+--
+-- Hint: use the function Data.Char.toLower that has been imported for you.
+--
+-- Examples:
+--   Name "Pekka" == Name "pekka"   ==> True
+--   Name "Pekka!" == Name "pekka"  ==> False
+
+data Name = Name String
+  deriving Show
+
+instance Eq Name where
+  (==) (Name a) (Name b) = map Data.Char.toLower a == map Data.Char.toLower b
+
+------------------------------------------------------------------------------
+-- Ex 4: here is a list type parameterized over the type it contains.
+-- Implement an instance "Eq (List a)" that compares the lists element
+-- by element.
+--
+-- Note how the instance needs an Eq a constraint. What happens if you
+-- remove it?
+
+data List a = Empty | LNode a (List a)
+  deriving Show
+
+instance Eq a => Eq (List a) where
+  (==) Empty Empty = True
+  (==) (LNode _ _) Empty = False
+  (==) Empty (LNode _ _) = False
+  (==) (LNode firstNode firstList) (LNode secondNode secondList) =
+    (firstNode == secondNode) && (firstList == secondList)
+
+------------------------------------------------------------------------------
+-- Ex 5: below you'll find two datatypes, Egg and Milk. Implement a
+-- type class Price, containing a function price. The price function
+-- should return the price of an item.
+--
+-- The prices should be as follows:
+-- * chicken eggs cost 20
+-- * chocolate eggs cost 30
+-- * milk costs 15 per liter
+--
+-- Example:
+--   price ChickenEgg  ==>  20
+
+data Egg = ChickenEgg | ChocolateEgg
+  deriving Show
+data Milk = Milk Int -- amount in litres
+  deriving Show
+
+class Price a where
+  price :: a -> Int
+
+instance Price Egg where
+  price ChickenEgg = 20
+  price ChocolateEgg = 30
+
+instance Price Milk where
+  price (Milk l) = l * 15
+
+------------------------------------------------------------------------------
+-- Ex 6: define the necessary instances in order to be able to compute these:
+--
+-- price (Just ChickenEgg) ==> 20
+-- price [Milk 1, Milk 2]  ==> 45
+-- price [Just ChocolateEgg, Nothing, Just ChickenEgg]  ==> 50
+-- price [Nothing, Nothing, Just (Milk 1), Just (Milk 2)]  ==> 45
+
+instance Price a => Price (Maybe a) where
+  price Nothing  = 0
+  price (Just x) = price x
+
+instance Price a => Price [a] where
+  price itemList = sum $ map price itemList
+
+------------------------------------------------------------------------------
+-- Ex 7: below you'll find the datatype Number, which is either an
+-- Integer, or a special value Infinite.
+--
+-- Implement an Ord instance so that finite Numbers compare normally,
+-- and Infinite is greater than any other value.
+
+data Number = Finite Integer | Infinite
+  deriving (Show,Eq)
+
+instance Ord Number where
+  (Finite a) <= (Finite b) = a <= b
+  Infinite <= (Finite _) = False
+  (Finite _) <= Infinite = True
+  Infinite <= Infinite = True
+
+------------------------------------------------------------------------------
+-- Ex 8: rational numbers have a numerator and a denominator that are
+-- integers, usually separated by a horizontal bar or a slash:
+--
+--      numerator
+--    -------------  ==  numerator / denominator
+--     denominator
+--
+-- You may remember from school that two rationals a/b and c/d are
+-- equal when a*d == b*c. Implement the Eq instance for rationals
+-- using this definition.
+--
+-- You may assume in all exercises that the denominator is always
+-- positive and nonzero.
+--
+-- Examples:
+--   RationalNumber 4 5 == RationalNumber 4 5    ==> True
+--   RationalNumber 12 15 == RationalNumber 4 5  ==> True
+--   RationalNumber 13 15 == RationalNumber 4 5  ==> False
+
+data RationalNumber = RationalNumber Integer Integer
+  deriving Show
+
+instance Eq RationalNumber where
+  (RationalNumber n1 d1) == (RationalNumber n2 d2) = n1*d2 == d1*n2
+
+------------------------------------------------------------------------------
+-- Ex 9: implement the function simplify, which simplifies rational a
+-- number by removing common factors of the numerator and denominator.
+-- In other words,
+--
+--     ca         a
+--    ----  ==>  ---
+--     cb         b
+--
+-- As a concrete example,
+--
+--     12        3 * 4         4
+--    ----  ==  -------  ==>  ---.
+--     15        3 * 5         5
+--
+-- Hint: Remember the function gcd?
+
+simplify :: RationalNumber -> RationalNumber
+simplify (RationalNumber num denom) = RationalNumber (num `div` gcdRes) (denom `div` gcdRes) where
+  gcdRes = gcd num denom
+
+------------------------------------------------------------------------------
+-- Ex 10: implement the typeclass Num for RationalNumber. The results
+-- of addition and multiplication must be simplified.
+--
+-- Reminders:
+--   * negate x is 0-x
+--   * abs is absolute value
+--   * signum is -1, +1 or 0 depending on the sign of the input
+--
+-- Examples:
+--   RationalNumber 1 3 + RationalNumber 1 6 ==> RationalNumber 1 2
+--   RationalNumber 1 3 * RationalNumber 3 1 ==> RationalNumber 1 1
+--   negate (RationalNumber 2 3)             ==> RationalNumber (-2) 3
+--   fromInteger 17 :: RationalNumber        ==> RationalNumber 17 1
+--   abs (RationalNumber (-3) 2)             ==> RationalNumber 3 2
+--   signum (RationalNumber (-3) 2)          ==> RationalNumber (-1) 1
+--   signum (RationalNumber 0 2)             ==> RationalNumber 0 1
+
+instance Num RationalNumber where
+  (RationalNumber n1 d1) + (RationalNumber n2 d2) = simplify $ RationalNumber (n1*d2 + n2*d1) (d1*d2)
+  (RationalNumber n1 d1) * (RationalNumber n2 d2) = simplify $ RationalNumber (n1*n2) (d1*d2)
+  abs (RationalNumber n d) = RationalNumber (pos n) (pos d) where
+    pos x = if x < 0 then negate x else x
+  signum (RationalNumber n d)
+    | n == 0 = 0
+    | n < 0 && d < 0 = 1
+    | n < 0 || d < 0 = -1
+    | otherwise = 1
+  fromInteger x = RationalNumber x 1
+  negate (RationalNumber n d) = RationalNumber (negate n) d
+
+------------------------------------------------------------------------------
+-- Ex 11: a class for adding things. Define a class Addable with a
+-- constant `zero` and a function `add`. Define instances of Addable
+-- for Integers and lists. Numbers are added with the usual addition,
+-- while lists are added by catenating them. Pick a value for `zero`
+-- such that: `add zero x == x`
+--
+-- Examples:
+--   add 1 2                ==>  3
+--   add 1 zero             ==>  1
+--   add [1,2] [3,4]        ==>  [1,2,3,4]
+--   add zero [True,False]  ==>  [True,False]
+
+class Addable a where
+  add :: a -> a -> a
+  zero :: a
+
+instance Addable Integer where
+  add x y = x + y
+  zero = 0
+
+instance Addable [a] where
+  add x y = x ++ y
+  zero = []
+
+------------------------------------------------------------------------------
+-- Ex 12: cycling. Implement a type class Cycle that contains a
+-- function `step` that cycles through the values of the type.
+-- Implement instances for Color and Suit that work like this:
+--
+--   step Red ==> Green
+--   step Green ==> Blue
+--   step Blue ==> Red
+--
+-- The suit instance should cycle suits in the order Club, Spade,
+-- Diamond, Heart, Club.
+--
+-- Also add a function `stepMany` to the class and give it a default
+-- implementation using `step`. The function `stepMany` should take
+-- multiple (determined by an Int argument) steps like this:
+--
+--   stepMany 2 Club ==> Diamond
+--   stepMany 3 Diamond ==> Spade
+--
+-- The tests will test the Cycle class and your default implementation
+-- of stepMany by adding an instance like this:
+--
+--    instance Cycle Int where
+--      step = succ
+
+data Color = Red | Green | Blue
+  deriving (Show, Eq)
+data Suit = Club | Spade | Diamond | Heart
+  deriving (Show, Eq)
+
+class Cycle a where
+  step :: a -> a
+  stepMany :: Int -> a -> a
+  stepMany 0 val = val
+  stepMany i val = stepMany (i-1) (step val)
+
+instance Cycle Color where
+  step c = case c of
+    Red -> Green
+    Green -> Blue
+    Blue -> Red
+
+instance Cycle Suit where
+  step s = case s of
+    Club -> Spade
+    Spade -> Diamond
+    Diamond -> Heart
+    Heart -> Club
